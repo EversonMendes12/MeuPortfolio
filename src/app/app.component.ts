f5c1f0a439e8689b69934cca20b1462f2e1010a0
everson oliveira mendes <eversonmendes301@gmail.com>
2024-05-22 13:56:27 -0300

    Corrige responsividade toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MeuPortfolio';
+  public isMenuOpen = false;
+
+  public toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
